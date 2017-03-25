1bb69c379d8751f31c8d2d38581debe05c32a175
Yooric2 <yooric90@hotmail.com>
2017-03-25 11:50:45 +0100

    la till i ett script

--- FysikProjekt/Assets/RocketScript.cs
+++ FysikProjekt/Assets/RocketScript.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RocketScript : MonoBehaviour {
+public class RocketScript : MonoBehaviour
+{
+    public float fuelPower = 1.0f;
+    public float hullWeight = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
