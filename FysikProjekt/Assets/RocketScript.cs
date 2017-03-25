5608c18be72c1876a07cbe134575a40661c90c5b
Slowey <lucas.holmqvist@gmail.com>
2017-03-25 13:21:41 +0100

    rocketssss

--- FysikProjekt/Assets/RocketScript.cs
+++ FysikProjekt/Assets/RocketScript.cs
@@ -5,11 +5,6 @@ using UnityEngine.UI;
 
 public class RocketScript : MonoBehaviour
 {
-    public float fuelPower = 1.0f;
-    public float hullMass = 1.0f;
-    public float fuelMass = 1.0f;
-    float velocity = 94.0f;
-    float alltitude = 0.0f;
     public Text exhaustUi;
     public Text hullMassUi;
     public Text fuelMasstUi;
@@ -21,25 +16,30 @@ public class RocketScript : MonoBehaviour
     public float m_startVelocity = 0f;
     public float m_exhaustVelocity = 2050;
 
-    public float m_fuelMass = 392400f;
-    public float m_hullMass = 20000f;
-    public float m_engineMass = 8371f;
+    //public float m_fuelMass = 392400f;
+    //public float m_hullMass = 20000f;
+    //public float m_engineMass = 8371f;
+    public float m_fuelMass = 400f;
+    public float m_hullMass = 400f;
+    public float m_engineMass = 0f;
     private float m_currentMass;
     private float m_startMass;
 
-    private float m_distanceFromEarthSurface;
+    private float m_distanceFromEarthSurface = 0f;
     private float m_earthRadius = 6356766.0f;
-    public float m_propellantBurnRate = 2616f; //kg/s (F-1 Engine stats from book)
+    //public float m_propellantBurnRate = 2616f; //kg/s (F-1 Engine stats from book)
+    public float m_propellantBurnRate = 0.5f;
     // Use this for initialization
     void Start()
     {
-        exhaustUi.text = "Exhaust velocity: " + fuelPower.ToString();//"Velocity: " + velocity.ToString() + " m/s";
-        hullMassUi.text = "Rocket mass: " + hullMass.ToString() + " kg";
-        velocityUi.text = "Velocity: " + velocity.ToString() + " m/s";
-        fuelMasstUi.text = "Fuel mass: " + fuelMass.ToString() + " kg";
-        alltitudeUi.text = "Alltitude: " + alltitude.ToString() + " m";
+        exhaustUi.text = "Exhaust velocity: " + m_exhaustVelocity.ToString();//"Velocity: " + velocity.ToString() + " m/s";
+        hullMassUi.text = "Rocket mass: " + m_currentMass.ToString() + " kg";
+        velocityUi.text = "Velocity: " + m_currentVelocity.ToString() + " m/s";
+        fuelMasstUi.text = "Fuel mass: " + m_fuelMass.ToString() + " kg";
+        alltitudeUi.text = "Alltitude: " + m_distanceFromEarthSurface.ToString() + " m";
 
         m_startMass = m_fuelMass + m_hullMass + m_engineMass;
+        m_currentMass = m_startMass;
 
     }
     // Update is called once per frame
@@ -52,14 +52,12 @@ public class RocketScript : MonoBehaviour
         }
         HandleGravity();
         UpdateRocketPosition();
-        alltitudeUi.text = "Alltitude: " + gameObject.transform.position.y + " m";
-    }
-    void SetMasses(float p_fuelMass, float p_hullMass, float p_engineMass)
-    {
-        m_fuelMass = p_fuelMass;
-        m_hullMass = p_hullMass;
-        m_engineMass = p_engineMass;
-        m_startMass = m_fuelMass + m_hullMass + m_engineMass;
+
+        hullMassUi.text = "Rocket mass: " + m_currentMass.ToString() + " kg";
+        velocityUi.text = "Velocity: " + m_currentVelocity.ToString() + " m/s";
+        fuelMasstUi.text = "Fuel mass: " + m_fuelMass.ToString() + " kg";
+        alltitudeUi.text = "Alltitude: " + m_distanceFromEarthSurface.ToString() + " m";
+        m_currentVelocity = 0f;
     }
     
     bool UpdateRocketMass()
@@ -67,7 +65,7 @@ public class RocketScript : MonoBehaviour
         // if rocketmass is larger than hullmass we still have fuel to burn
         if (m_currentMass > m_hullMass)
         {
-            m_currentMass = m_currentMass - m_propellantBurnRate * Time.deltaTime;
+            m_currentMass = m_currentMass - m_propellantBurnRate * Time.deltaTime * 100f;
             return true;
         }
         else
@@ -86,7 +84,7 @@ public class RocketScript : MonoBehaviour
     void HandleGravity()
     {
         // a * t = v.  a = 9.81*(re^2/(re+h)^2)
-        m_currentVelocity -= (9.81f * (Mathf.Pow(m_earthRadius, 2) / (Mathf.Pow(m_earthRadius + m_distanceFromEarthSurface, 2))))* Time.deltaTime;
+        m_currentVelocity -= (9.81f * (Mathf.Pow(m_earthRadius, 2) / (Mathf.Pow(m_earthRadius + m_distanceFromEarthSurface, 2))))* Time.deltaTime * 100f;
     }
     void HandleThrust()
     {
