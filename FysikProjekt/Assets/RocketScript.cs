090dc2aa24eacacb55cf61e121c1591a9fccdde8
Yooric2 <yooric90@hotmail.com>
2017-03-30 16:47:42 +0200

    added some things. escpaveelcootiy grejen i script. o scenne

--- FysikProjekt/Assets/RocketScript.cs
+++ FysikProjekt/Assets/RocketScript.cs
@@ -27,12 +27,15 @@ public class RocketScript : MonoBehaviour
     public float m_engineMass = 0f;
     private float m_currentMass;
     private float m_startMass;
+    private float m_escapeVelocity = 0;
 
     private float m_distanceFromEarthSurface = 0f;
     private float m_earthRadius = 6356766.0f;
+    private float m_earthMass = 5973600000000000000000000.0f;
+    private float m_gravitationalConstant = 6.67f * Mathf.Pow(10, -11);
     //public float m_propellantBurnRate = 2616f; //kg/s (F-1 Engine stats from book)
     public float m_propellantBurnRate = 0.5f;
-    private float m_worldScale= 0.01f;
+    private float m_worldScale= 0.00001f;
     public float m_velGainScale = 100;
     // Use this for initialization
     void Start()
@@ -42,8 +45,9 @@ public class RocketScript : MonoBehaviour
         velocityUi.text = "Velocity: " + m_currentVelocity.ToString() + " m/s";
         fuelMasstUi.text = "Fuel mass: " + m_fuelMass.ToString() + " kg";
         alltitudeUi.text = "Alltitude: " + m_distanceFromEarthSurface.ToString() + " m";
-        gravity.text = "g: " + 9.81;
-        escapeVelocity.text = "Escape velocity: " + 10;
+        // gravity.text = "g: " + 9.81;
+        EscapeVelocity();
+        escapeVelocity.text = "Escape velocity: " + m_escapeVelocity;
 
         m_startMass = m_fuelMass + m_hullMass + m_engineMass;
         m_currentMass = m_startMass;
@@ -57,21 +61,25 @@ public class RocketScript : MonoBehaviour
         {
             HandleVelocity();
         }
-        if(m_escVeloReached)
+        if(m_currentVelocity>m_escapeVelocity)
         {
             escapeVeloReached.text = "Escape velocity reached";
         }
-        if (!m_escVeloReached)
+        else
         {
-            escapeVeloReached.text = "";
+            escapeVeloReached.text = "Escape velocity not reached";
         }
         HandleGravity();
         UpdateRocketPosition();
+        EscapeVelocity();
 
         hullMassUi.text = "Rocket mass: " + m_currentMass.ToString() + " kg";
         velocityUi.text = "Velocity: " + m_currentVelocity.ToString() + " m/s";
         fuelMasstUi.text = "Fuel mass: " + m_fuelMass.ToString() + " kg";
         alltitudeUi.text = "Alltitude: " + m_distanceFromEarthSurface.ToString() + " m";
+
+        escapeVelocity.text = "Escape velocity: " + m_escapeVelocity.ToString() + " m";
+
     }
     
     bool UpdateRocketMass()
@@ -95,6 +103,10 @@ public class RocketScript : MonoBehaviour
         // might seem a little odd to update this variable but it makes the code easier to read in HandleGravity()
         m_distanceFromEarthSurface = transform.position.y* (1/ m_worldScale);
     }
+    void EscapeVelocity()
+    {
+        m_escapeVelocity = Mathf.Sqrt((2 * m_gravitationalConstant * m_earthMass )/ (m_earthRadius + m_distanceFromEarthSurface));
+    }
     void HandleGravity()
     {
         // a * t = v.  a = 9.81*(re^2/(re+h)^2)
@@ -107,7 +119,7 @@ public class RocketScript : MonoBehaviour
 
         double adbl = earthradSqrddbl / distanceearthcoreSqrddbl * 9.81;
         float a = earthradiusSquared / distancefromEarthCoreSquared * 9.81f;
-
+        gravity.text = "g: " + (9.81f * (Mathf.Pow(m_earthRadius, 2) / (Mathf.Pow(m_earthRadius + m_distanceFromEarthSurface, 2))));
         m_currentVelocity -= (9.81f * (Mathf.Pow(m_earthRadius, 2) / (Mathf.Pow(m_earthRadius + m_distanceFromEarthSurface, 2)))) * Time.deltaTime * m_velGainScale;
         //}
         //else
